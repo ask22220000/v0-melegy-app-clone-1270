ebd8a29ae241b9d252762f2c267d67e19eec3932
ask22220000 <brushdesigns3@gmail.com>
2026-02-13 22:41:13 +0200

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  skipTrailingSlashRedirect: true,
+  productionBrowserSourceMaps: false,
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+}
+
+export default nextConfig
